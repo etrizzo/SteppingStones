c5beca9be5671228c0b9a536926012040f4c9d84
Linnea Kirby <linneakirby@gmail.com>
2016-04-11 16:29:20 -0400

    add preliminary sound

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -6,14 +6,26 @@ public class GameManager : MonoBehaviour {
 
 
 	SquareManager sqman;
+	public AudioSource gameAudio;
+	public AudioClip gameClip;
 
 	void Start () {
 		GameObject sqmanObject = new GameObject ();
 		sqman = sqmanObject.AddComponent<SquareManager> ();
 		sqman.name = "Square Manager";
 		sqman.init ();
+
+		initSound ();
 	}
 
+	void initSound(){
+		gameAudio = this.gameObject.AddComponent<AudioSource> ();
+		gameAudio.loop = true;
+		gameAudio.playOnAwake = false;
+		gameClip = Resources.Load<AudioClip> ("Audio/Soundtrack Draft 1");
+		gameAudio.clip = gameClip;
+		gameAudio.Play ();
+	}
 
 	void Update(){
 		if (Input.GetMouseButtonUp(0)) {

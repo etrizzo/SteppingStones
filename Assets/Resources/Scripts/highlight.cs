18fadda3acceb17433e60b5f9db6f5fab4e329c1
Andres Cuervo <andrescuervor@gmail.com>
2016-04-28 02:46:34 -0400

    Fix coloring of horizontal highlight blocks

--- Assets/Resources/Scripts/highlight.cs
+++ Assets/Resources/Scripts/highlight.cs
@@ -18,8 +18,17 @@ public class Highlight : MonoBehaviour {
 	Square[] rigidShapes;
 	int rigidYOffset = -10;
 
+
 	// Save our color here so it's consistent :)
 	Color color = Color.gray;
+
+	public Square addHighlightSquare(int x,int  y) {
+		Square sq = sqman.addSquare (new Vector2 (x, y), false);
+		sq.model.mat.color = color;
+		sq.setType (0);
+		sq.model.gameObject.name = "Highlight Square";
+		return sq;
+	}
 
 	public void init(Queue<Square> q, SquareManager sqm) {
 		sqman = sqm;
@@ -28,17 +37,15 @@ public class Highlight : MonoBehaviour {
 		queue = q;
 
 		// Init made actually lolololol
-		made = sqman.addSquare(new Vector2(mouse.x, mouse.y), false);
-		made.model.mat.color = color;
-		made.setType (0);
-		made.model.gameObject.name = "Highlight Square";
+		made = addHighlightSquare ((int) mouse.x, (int) mouse.y);
+
 
 		rigidShapes = new Square[EXTRASHAPES];
 
 		// Make the rigid blocks even if we don't need them lol
 		for (int i = 0; i < EXTRASHAPES; i++) {
 			// TODO: Need to replace addSquare with a addHighlightSquare method :))))
-			rigidShapes[i] = sqman.addSquare(new Vector2((int) mouse.x + i + 1, rigidYOffset), false);
+			rigidShapes[i] = addHighlightSquare((int) mouse.x + i + 1, rigidYOffset);
 		}
 
 
@@ -62,17 +69,30 @@ public class Highlight : MonoBehaviour {
 	}
 
 	void updateModel() {
-		int lowestY = getLowestYCoord ();
+//		Debug.Log ("Got the queue, it looks like: " + queue.Count);
+		Square next = sqman.queue.Peek ();
+		int type = next.getType ();
+
+		int lowestY = getLowestYCoordForColumn ((int) mouse.x);
+
+//		if (type == 5) {
+//			// If it's a rigid shape, we have to get the lowest possible y for all the rigids
+//			foreach (Square sq in rigidShapes) {
+//				int this_y = (int) sq.getPosition ().y;
+//				if (this_y > lowestY) {
+//					Debug.Log ("Changing this_y to:" + this_y);
+//					lowestY = this_y;
+//					color = Color.red;
+//				}
+//			}
+//		}
+
 		// If we're over the board do the things
 //		Debug.Log("Lowest y coordinate is: " + lowestY);
 		if (lowestY != -1) {
-			made.model.mat.color = color;
 			if (mouseMoved()) {
 				// If the mouse moved, update the position of the anchor
 				made.transform.position = new Vector3(mouse.x, lowestY, 0);
-//				Debug.Log ("Got the queue, it looks like: " + queue.Count);
-				Square next = sqman.queue.Peek ();
-				int type = next.getType ();
 //				Debug.Log ("Next queued block is of type: " + type);
 				if (type == 5) {
 					// If it's a RigidShape, do extra things based on what kind of shape it is!
@@ -82,6 +102,20 @@ public class Highlight : MonoBehaviour {
 						// it's _, spawn some additional shapes!
 						for (int i = 0; i <= EXTRASHAPES; i++) {
 							rigidShapes [i].transform.position = new Vector3 (mouse.x + i + 1, lowestY, 0);
+
+//							foreach (Square sq in rigidShapes) {
+//								int this_y = (int) sq.getPosition ().y;
+//								// TODO: Okay, no, you actually have to check that this is occupied, and then get the y in this
+//								// column :////////////
+//								if (squareAtYCoord((int) mouse.x, this_y) != null) {
+//									Debug.Log ("Changing this_y to:" + this_y);
+//									lowestY = this_y;
+//									color = Color.red;
+//								}
+//							}
+//
+//							rigidShapes [i].transform.position = new Vector3 (mouse.x + i + 1, lowestY, 0);
+
 							// TODO: Need to replace add
 //							rigidShapes[i] = sqman.addSquare(new Vector2((int) mouse.x + i + 1, lowestY), false);
 						}
@@ -91,13 +125,14 @@ public class Highlight : MonoBehaviour {
 					// We know this is an anchor
 
 				} else {
+					color = Color.gray;
 					// Reset the anchor highlight blocks to be off the screen when we don't need them!
 					for (int i = 0; i <= EXTRASHAPES; i++) {
 						rigidShapes [i].transform.position = new Vector3 (mouse.x + i + 1, rigidYOffset, 0);
 						// TODO: Need to replace add
 					}
 				}
-
+			made.model.mat.color = color;
 			}
 		} else {
 			// If we're **not over the board**, make the square transparent lol
@@ -111,11 +146,11 @@ public class Highlight : MonoBehaviour {
 		return lastMousePos == mouse;
 	}
 
-	private int getLowestYCoord() {
+	private int getLowestYCoordForColumn(int x) {
 		int highestBlockY = (int) mouse.y;
 
 		// Search down from our mouse.y for the first not null thing
-		while (squareAtYCoord(highestBlockY) == null && highestBlockY >= 0) {
+		while (squareAtYCoord(x, highestBlockY) == null && highestBlockY >= 0) {
 			highestBlockY--;
 		}
 
@@ -123,7 +158,7 @@ public class Highlight : MonoBehaviour {
 		// But if it's equal to where we started, we got a problem, sooooo
 		if (highestBlockY == (int) mouse.y) {
 			// ... check, going up from the current block, for the first null block to put it in
-			while (squareAtYCoord(highestBlockY) != null && highestBlockY <= sqman.BOARDSIZEY - 1) {
+			while (squareAtYCoord(x, highestBlockY) != null && highestBlockY <= sqman.BOARDSIZEY - 1) {
 				highestBlockY++;
 			}
 			return highestBlockY;
@@ -134,7 +169,7 @@ public class Highlight : MonoBehaviour {
 		return -1;
 	}
 
-	private Square squareAtYCoord(int y) {
-		return board[(int) mouse.x, y];
+	private Square squareAtYCoord(int x, int y) {
+		return board[x, y];
 	}
 }
